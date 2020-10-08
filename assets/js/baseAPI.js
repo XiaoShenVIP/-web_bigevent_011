40ae8415b7a30e86be2fec3178818bb791828577
sky <vipshenguanyun@163.com>
2020-10-09 00:16:19 +0800

    登录页面初始化

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,23 @@ var baseURL = "http://ajax.frontend.itheima.net";
 
 $.ajaxPrefilter(function (params) {
     params.url = baseURL + params.url;
-    console.log(params);
+    // console.log(params);
+
+    // 2.权限接口  配置头信息
+    if (params.url.indexOf('/my/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token" || "")
+        }
+    }
+    // 3.身份认证判断
+    params.complete = function (res) {
+        // console.log(res);
+        // 权限认证
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.删除本地 token
+            localStorage.removeItem("token");
+            // 2.页面跳转
+            location.href = '/login.html';
+        }
+    }
 })
